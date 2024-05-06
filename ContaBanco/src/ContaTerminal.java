edc7ce845d4b1fe55ae2f8db750ddff0686b7ca4
Luciano Santos <lucianosantosdti@gmail.com>
2024-05-06 14:43:22 -0300

    Codigo Desafio Conta

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Olá seja bem vindo(a) ao seu banco");
+        System.out.println("################################");
+        System.out.println("Informe o  número da agência: ");
+        int numero = s.nextInt();
+        s.nextLine();
+        System.out.println("Informe o código da sua agência: ");
+        String agencia = s.nextLine();
+
+        System.out.println("Informe o seu nome: ");
+        String nome = s.nextLine();
+
+        System.out.println("Informe o saldo da conta: ");
+        double saldo = s.nextDouble();
+
+        System.out.println("Olá "+nome+" ,obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo de R$:140"+saldo+" já está disponível para saque.");
     }
 }
