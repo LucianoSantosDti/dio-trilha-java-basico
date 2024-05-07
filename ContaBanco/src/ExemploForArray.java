92e0443e3c6ffe06c62090bd0766e37195779073
Luciano Santos <lucianosantosdti@gmail.com>
2024-05-07 14:44:58 -0300

    Repeticao

---
+++ ContaBanco/src/ExemploForArray.java
@@ -0,0 +1,8 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe", "Cláudio","José","Pedro"};
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no índice "+x+" é o aluno : "+alunos[x]);
+        }
+    } 
+}
